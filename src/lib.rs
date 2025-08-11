27a2ebadd7caf83a2e8ccd9014bfb1bbce4e7d49
youness <marvelbark2@gmail.com>
2025-08-11 09:35:18 +0200

    refactor: integrate AppCommandParser into handle_stream and remove redundant parsing functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::{self, BufRead};
+
 pub enum AppCommand {
     Ping,
     Echo(String),
@@ -36,5 +38,72 @@ impl AppCommand {
             "EXISTS" if parts.len() > 1 => Some(AppCommand::Exists(parts[1].clone())),
             _ => None,
         }
+    }
+}
+
+pub struct AppCommandParser {
+    line: String,
+}
+
+impl AppCommandParser {
+    pub fn new() -> Self {
+        AppCommandParser {
+            line: String::new(),
+        }
+    }
+    pub fn parse_resp_array<R: BufRead>(mut self: Self, reader: &mut R) -> io::Result<Vec<String>> {
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+
+        if !self.line.starts_with('*') {
+            self.line = line;
+            return self.parse_simple();
+        }
+        let count: usize = self.line[1..]
+            .trim()
+            .parse()
+            .map_err(|_| io::ErrorKind::InvalidData)?;
+
+        let mut parts = Vec::with_capacity(count);
+
+        for _ in 0..count {
+            let mut line = String::new();
+            reader.read_line(&mut line)?;
+            if !line.starts_with('$') {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Expected bulk string",
+                ));
+            }
+            let len: usize = line[1..]
+                .trim()
+                .parse()
+                .map_err(|_| io::ErrorKind::InvalidData)?;
+
+            let mut buf = vec![0u8; len];
+            reader.read_exact(&mut buf)?;
+            parts.push(str::from_utf8(&buf).unwrap().to_string());
+
+            // Consume trailing \r\n
+            let mut crlf = [0u8; 2];
+            reader.read_exact(&mut crlf)?;
+        }
+
+        Ok(parts)
+    }
+
+    pub fn parse_simple(self: &Self) -> io::Result<Vec<String>> {
+        let line = &self.line;
+        if line.is_empty() {
+            return Ok(vec![]);
+        }
+
+        let parts: Vec<String> = line
+            .trim()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+        println!("Parsed parts: {:?}", parts);
+        Ok(parts)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::{
     thread,
 };
 
-use codecrafters_redis::AppCommand;
+use codecrafters_redis::{AppCommand, AppCommandParser};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -32,9 +32,10 @@ fn main() {
 
 fn handle_stream(mut stream: TcpStream) -> io::Result<()> {
     let mut reader = BufReader::new(stream.try_clone().unwrap());
-
     loop {
-        let cmd_parts = parse_resp_array(&mut reader)?;
+        let command_parser = AppCommandParser::new();
+
+        let cmd_parts = command_parser.parse_resp_array(&mut reader)?;
         if cmd_parts.is_empty() {
             continue;
         }
@@ -53,58 +54,4 @@ fn handle_stream(mut stream: TcpStream) -> io::Result<()> {
             }
         }
     }
-}
-
-fn parse_resp_array<R: BufRead>(reader: &mut R) -> io::Result<Vec<String>> {
-    let mut line = String::new();
-
-    reader.read_line(&mut line)?;
-    if !line.starts_with('*') {
-        return parse_simple(&mut line);
-    }
-    let count: usize = line[1..]
-        .trim()
-        .parse()
-        .map_err(|_| io::ErrorKind::InvalidData)?;
-
-    let mut parts = Vec::with_capacity(count);
-
-    for _ in 0..count {
-        line.clear();
-        reader.read_line(&mut line)?;
-        if !line.starts_with('$') {
-            return Err(io::Error::new(
-                io::ErrorKind::InvalidData,
-                "Expected bulk string",
-            ));
-        }
-        let len: usize = line[1..]
-            .trim()
-            .parse()
-            .map_err(|_| io::ErrorKind::InvalidData)?;
-
-        let mut buf = vec![0u8; len];
-        reader.read_exact(&mut buf)?;
-        parts.push(str::from_utf8(&buf).unwrap().to_string());
-
-        // Consume trailing \r\n
-        let mut crlf = [0u8; 2];
-        reader.read_exact(&mut crlf)?;
-    }
-
-    Ok(parts)
-}
-
-fn parse_simple(line: &mut String) -> io::Result<Vec<String>> {
-    if line.is_empty() {
-        return Ok(vec![]);
-    }
-
-    let parts: Vec<String> = line
-        .trim()
-        .split_whitespace()
-        .map(|s| s.to_string())
-        .collect();
-    println!("Parsed parts: {:?}", parts);
-    Ok(parts)
 }
